dd87f038d1c0fb53fe705d4547d63c3d40f4ad96
arakno <paulobasto@gmail.com>
2016-04-22 22:17:06 +0100

    delete bt UI fix. Added remove All from cart button. Normalized code convention (Class, instance)

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ an angular shopping cart
 
 ## Getting Started
 
-To get you started you can simply clone the git@github.com:arakno/shoppingCart-ng.git repository and install the dependencies:
+To get you started you can simply clone git@github.com:arakno/shoppingCart-ng.git repository and install the dependencies:
 
 ### Install dependencies
 ```
--- app/js/services.js
+++ app/js/services.js
@@ -5,7 +5,7 @@ angular.module('myApp.services', [])
   .service('productService', ['$q', function ($q) {
 
     // product class
-    function product(sku, name, description, price) {
+    function Product(sku, name, description, price) {
       this.sku = sku;
       this.name = name;
       this.description = description;
@@ -13,28 +13,28 @@ angular.module('myApp.services', [])
     }
 
 
-    var Products = [
-      new product("APL", "Apple", "Eat one every day to keep the doctor away!", 12),
-      new product("AVC", "Avocado", "Guacamole anyone?", 16),
-      new product("BAN", "Banana", "These are rich in Potassium and easy to peel.", 4),
-      new product("CTP", "Cantaloupe", "Delicious and refreshing.", 3),
-      new product("FIG", "Fig", "OK, not that nutritious, but sooo good!", 10),
-      new product("GRF", "Grapefruit", "Pink or red, always healthy and delicious.", 11),
-      new product("GRP", "Grape", "Wine is great, but grapes are even better.", 8),
-      new product("GUA", "Guava", "Exotic, fragrant, tasty!", 8),
-      new product("KIW", "Kiwi", "These come from New Zealand.", 14),
-      new product("LYC", "Lychee", "Unusual and highly addictive!", 18),
-      new product("MAN", "Mango", "Messy to eat, but well worth it.", 8),
-      new product("ORG", "Orange", "Vitamin C anyone? Go ahead, make some juice.", 9),
-      new product("PAP", "Papaya", "Super-popular for breakfast.", 5),
-      new product("PCH", "Peach", "Add some cream and enjoy.", 19),
-      new product("PER", "Pear", "Delicious fresh, or cooked in red wine, or distilled.", 8),
-      new product("PMG", "Pomegranate", "Delicious, healthy, beautiful, and sophisticated!", 19),
-      new product("PNP", "Pineapple", "Enjoy it (but don't forget to peel first).", 4)
+    var products = [
+      new Product("APL", "Apple", "Eat one every day to keep the doctor away!", 12),
+      new Product("AVC", "Avocado", "Guacamole anyone?", 16),
+      new Product("BAN", "Banana", "These are rich in Potassium and easy to peel.", 4),
+      new Product("CTP", "Cantaloupe", "Delicious and refreshing.", 3),
+      new Product("FIG", "Fig", "OK, not that nutritious, but sooo good!", 10),
+      new Product("GRF", "Grapefruit", "Pink or red, always healthy and delicious.", 11),
+      new Product("GRP", "Grape", "Wine is great, but grapes are even better.", 8),
+      new Product("GUA", "Guava", "Exotic, fragrant, tasty!", 8),
+      new Product("KIW", "Kiwi", "These come from New Zealand.", 14),
+      new Product("LYC", "Lychee", "Unusual and highly addictive!", 18),
+      new Product("MAN", "Mango", "Messy to eat, but well worth it.", 8),
+      new Product("ORG", "Orange", "Vitamin C anyone? Go ahead, make some juice.", 9),
+      new Product("PAP", "Papaya", "Super-popular for breakfast.", 5),
+      new Product("PCH", "Peach", "Add some cream and enjoy.", 19),
+      new Product("PER", "Pear", "Delicious fresh, or cooked in red wine, or distilled.", 8),
+      new Product("PMG", "Pomegranate", "Delicious, healthy, beautiful, and sophisticated!", 19),
+      new Product("PNP", "Pineapple", "Enjoy it (but don't forget to peel first).", 4)
     ];
 
     this.getAll = function () {
-      return $q.when({ data: Products });
+      return $q.when({ data: products });
     }
 
 
@@ -43,25 +43,28 @@ angular.module('myApp.services', [])
     var items = [];
 
     function findProduct(product) {
-      var matched;
       angular.forEach(items, function (item) {
         if (item.product.sku == product.sku) {
-          matched = item;
-          return;
+          return item;
         }
       });
-      return matched;
     }
 
     this.getItems = function () {
       return items;
     }
 
-    this.removeItem = function (item) {
+    this.removeItem = function (item, e) {
       var index = items.indexOf(item);
       items.splice(index, 1);
+      e.preventDefault();
     }
-
+    
+    this.removeAll = function (e) {
+      items.length = 0;
+      e.preventDefault();
+    }
+    
     this.total = function (delivery) {
       var sum = 0;
       angular.forEach(items, function (item) {
@@ -78,7 +81,6 @@ angular.module('myApp.services', [])
 
       var item = findProduct(product);
 
-
       if (item) {
         item.quantity += quantity;
       } else {
@@ -94,18 +96,18 @@ angular.module('myApp.services', [])
     }
   }).service('delivery', ['$q', function ($q) {
 
-    function delivery(name, cost) {
+    function Delivery(name, cost) {
       this.name = name;
       this.cost = cost;
     }
-
-    var Deliveries = [
-      new delivery("deliveroo", 20),
-      new delivery("DHL", 10)
+    
+    var deliveries = [
+      new Delivery("deliveroo", 20),
+      new Delivery("DHL", 10)
     ];
 
     this.getAll = function () {
-      return $q.when({ data: Deliveries });
+      return $q.when({ data: deliveries });
     }
 
   }]);
